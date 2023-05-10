0d695e7ee4bb6c477c6d381cd90b4de61307ed22
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2023-05-10 09:21:08 +0700

    menambahkan file uts

---
+++ cardMenu.php
@@ -0,0 +1,107 @@
+<div class="container-fluid">
+    <div class="text-center mb-3 mt-5">
+        <h1>Choose & Enjoy</h1>
+        <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/23/3492973024.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem</p>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center mb-5 mt-5">
+        <a href="#" class="btn btn-primary">View All</a>
+    </div>
+</div>
