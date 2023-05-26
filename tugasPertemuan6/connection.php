45971abc94acbd0e045d6fbc6bd4943290950292
muhammadramdani323 <93460086+muhammadramdani323@users.noreply.github.com>
2023-05-26 23:54:09 +0700

    Delete tugasPertemuan6 directory

--- tugasPertemuan6/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Variabel $conn digunakan untuk menyimpan koneksi ke database
-$conn = mysqli_connect('localhost', 'root', '', 'dbaccountsjuga') or die("Database not found");
-// Fungsi mysqli_connect() digunakan untuk membuat koneksi ke database MySQL
